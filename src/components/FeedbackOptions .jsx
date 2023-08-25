24f30f7c6a1fb1e72f57a9f513a35e29e02ef057
arhip14 <kruvdyk1503@gmail.com>
2023-08-25 17:43:28 +0300

    feadbackoptions edit

--- src/components/FeedbackOptions .jsx
+++ src/components/FeedbackOptions .jsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import '../index.css'; 
+import styled from 'styled-components';
+
+
+const Button = styled.button`
+ padding: 10px 20px;
+ font-size: 16px;
+ margin: 5px;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+   color: white;
+  transition: background-color 0.3s ease;
+  background-color: #e67e22;
+   &:hover {
+   background-color: #d35400;
+}
+`;
+
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
   <div>
     {options.map((option) => (
-      <button
+      <Button
         key={option}
-        className="button feedback-button"
         onClick={() => onLeaveFeedback(option)}
       >
         {option}
-      </button>
+      </Button>
     ))}
   </div>
 );
+
 
 export default FeedbackOptions;
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -10,7 +10,6 @@ const Statistics = ({ feedback }) => {
 
   return (
     <div>
-      <h2>Statistics</h2>
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
