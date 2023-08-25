3394095b2b8d00642f47c9744647e8123cd04be2
arhip14 <kruvdyk1503@gmail.com>
2023-08-25 17:50:18 +0300

    edit statistics.jsx

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const StatisticsContainer = styled.div`
+  text-align: center;
+  margin-top: 20px;
+`;
+
+const NoFeedback = styled.p`
+  font-style: italic;
+  color: #777;
+`;
+
 
 const Statistics = ({ feedback }) => {
   const countTotalFeedback = () => Object.values(feedback).reduce((acc, value) => acc + value, 0);
   const countPositiveFeedbackPercentage = () => (feedback.good / countTotalFeedback()) * 100 || 0;
 
-  if (countTotalFeedback() === 0) {
-    return <p>There is no feedback yet.</p>;
-  }
-
   return (
-    <div>
-      <p>Good: {feedback.good}</p>
-      <p>Neutral: {feedback.neutral}</p>
-      <p>Bad: {feedback.bad}</p>
-      <p>Total: {countTotalFeedback()}</p>
-      <p>Positive Percentage: {countPositiveFeedbackPercentage()}%</p>
-    </div>
+    <StatisticsContainer>
+      {countTotalFeedback() === 0 ? (
+        <NoFeedback>There is no feedback yet.</NoFeedback>
+      ) : (
+        <>
+          <p>Good: {feedback.good}</p>
+          <p>Neutral: {feedback.neutral}</p>
+          <p>Bad: {feedback.bad}</p>
+          <p>Total: {countTotalFeedback()}</p>
+          <p>Positive Percentage: {countPositiveFeedbackPercentage()}%</p>
+        </>
+      )}
+    </StatisticsContainer>
   );
 };
 
 export default Statistics;
+
+
